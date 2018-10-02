aab950b31198a0be7dd4d1bd6e6b3332fb883ab6
Farris <35810282+FarrisSwisher@users.noreply.github.com>
2018-10-01 19:57:09 -0500

    Add files via upload

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { ExplorePage } from '../explore/explore';
+import { SearchPage } from '../search/search';
+import { FeedPage } from '../feed/feed';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = FeedPage;
+  tab2Root = ExplorePage;
+  tab3Root = SearchPage;
+  tab4Root = ProfilePage;
 
   constructor() {
 
