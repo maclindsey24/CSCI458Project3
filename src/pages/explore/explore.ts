15d47b2d87f5ef33496b6f70bf51f1f6ceb743fe
maclindsey24 <43348704+maclindsey24@users.noreply.github.com>
2018-10-01 21:04:41 -0500

    Update explore.ts

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SettingsPage } from "../settings/settings"
+import { SettingsPage } from "../settings/settings";
 
 @Component({
   selector: 'page-explore',
