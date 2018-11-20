5c905eca7d1801aa486452b1c9047ef20a09b334
Farris <35810282+FarrisSwisher@users.noreply.github.com>
2018-11-20 16:24:49 -0600

    Add files via upload

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { User } from "../../models/user";
 import { AngularFireAuth } from "angularfire2/auth";
 
@@ -19,19 +19,23 @@ export class LoginPage {
 
   user = {} as User;
 
-  constructor(private afAuth: AngularFireAuth,
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController,
     public navCtrl: NavController, public navParams: NavParams) {
   }
 
   async login(user: User) {
     try{
-      const result = this.afAuth.auth.signInWithEmailAndPassword(user.email,user.password);
+      const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email,user.password);
       console.log(result);
       if (result){
-        this.navCtrl.push('tabs-page');
+        this.navCtrl.setRoot('tabs-page');
       }
     }
     catch (e){
+      this.toast.create({
+        message: 'Wrong email or password!',
+        duration: 3000
+      }).present();
       console.error(e);
     }
   }
